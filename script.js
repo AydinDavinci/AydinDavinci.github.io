945c2c3ab7d82348eb445ed8a48ade88e502248c
Aydin <143701285+AydinDavinci@users.noreply.github.com>
2025-03-27 13:25:16 +0100

    add lingo project

---
+++ script.js
@@ -0,0 +1,19 @@
+
+function changeToEnglish() {
+    document.getElementById('intro').innerHTML = `
+        My name is Aydin Kirat, I'm 18 years old.<br>
+        I'm very passionate about IT and pursuing a Software Developer education.<br><br>
+        I am creative and ambitious, thanks to my previous work experiences I can communicate well and work
+        attentively.
+        <br>In addition, I am proficient in both Dutch and English language.
+    `;
+    document.getElementById('skillTitle').innerHTML = '『Skills』';
+    document.getElementById('softwareText').innerHTML = `I like to code in different languages, I started with HTML in 2020, and after my first education, I immediately persuied a Software Developer Education.`;
+    document.getElementById('3dmodelText').innerHTML = `In my free time I also like to create 3D models, for this, I use softwares like Blender and Quixel Mixer. Usually these are personal projects`;
+    document.getElementById('hardwareText').innerHTML = `Hardware is also one of the things that attract me to IT, in 2023 I built my own gaming PC en had previous internships about hardware`;
+    document.getElementById('physicalText').innerHTML = `If I've got time, I like to do pshyical activities, alone or with friends. This is a way for me to not only be busy with IT`
+    document.getElementById('historyText').innerHTML = `Since young, I've also been interested in history, especially military history. I also build scale models about this subject and other diverse creative things.`
+    document.getElementById('hardwareText').innerHTML = `And of course, last but not least, I also enjoy playing video games! From shooters to platformers I could play anything and have fun with it.`
+
+
+}
